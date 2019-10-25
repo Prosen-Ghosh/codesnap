9e1b6b800cba5d3981d728a2590a2812839cb908
Prosen Ghosh <prosenghosh25@gmail.com>
2019-10-25 15:27:52 +0600

    code formated

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -2,24 +2,22 @@ import React from 'react';
 import { rhythm } from '../utils/typography';
 
 export default function Badge({ data }) {
-  return data.map(v => {
-    return (
-      <span
-        key={v}
-        style={{
-          backgroundColor: '#0000001f',
-          padding: '1px 3px 1px 3px',
-          margin: '3px 3px',
-          borderRadius: '4px',
-          color: '#0000008a',
-          fontSize: rhythm(0.4),
-          textAlign: 'center',
-          lineHeight: 1.2,
-          display: 'inline-block',
-        }}
-      >
-        {v}
-      </span>
-    );
-  });
+  return data.map(v => (
+    <span
+      key={v}
+      style={{
+        backgroundColor: '#0000001f',
+        padding: '1px 3px 1px 3px',
+        margin: '3px 3px',
+        borderRadius: '4px',
+        color: '#0000008a',
+        fontSize: rhythm(0.4),
+        textAlign: 'center',
+        lineHeight: 1.2,
+        display: 'inline-block',
+      }}
+    >
+      {v}
+    </span>
+  ));
 }
